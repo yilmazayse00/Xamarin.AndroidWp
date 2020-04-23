e7fd1510f7fdf7478b89ff685cbed0f326682d72
yilmazayse00 <32266594+yilmazayse00@users.noreply.github.com>
2020-04-23 15:48:50 +0300

    Add files via upload

--- XamarinUniversity/CreateChat.cs
+++ XamarinUniversity/CreateChat.cs
@@ -15,12 +15,48 @@ namespace XamarinUniversity
     [Activity(Label = "Create Chat")]
     public class CreateChat : Activity
     {
+        public static readonly int PickImageId = 1000;
+        private ImageView _imageView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.NewChat);
+            
+            _imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+            FindViewById<Button>(Resource.Id.button2).Click += OnPickImage;
+            FindViewById<Button>(Resource.Id.button1).Click += OnAddContact;
 
-            // Create your application here
+        }
+
+        private void OnAddContact(object sender, EventArgs e)
+        {
+            string name = FindViewById<EditText>(Resource.Id.addName).Text;
+
+            var intent = new Intent();
+
+            intent.PutExtra("Name", name);
+            SetResult(Result.Ok, intent);
+
+            Finish();   
+        }
+
+        private void OnPickImage(object sender, EventArgs e)
+        {
+            Intent = new Intent();
+            Intent.SetType("image/*");
+            Intent.SetAction(Intent.ActionGetContent);
+            StartActivityForResult(Intent.CreateChooser(Intent, "Select Picture"), PickImageId);
+        }
+
+  
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            if ((requestCode == PickImageId) && (resultCode == Result.Ok) && (data != null))
+            {
+                Android.Net.Uri uri = data.Data;
+                _imageView.SetImageURI(uri);
+            }
         }
     }
 }
+
